8810a5ac15ddf741352695a49eb258ce903f22fa
NicoJmn <Nicolas.jeanmenne@student.uclouvain.be>
2021-03-23 12:00:47 +0100

    delete cluster.c

--- src/cluster.c
+++
@@ -1,8 +0,0 @@
-#include "../headers/cluster.h"
-
-#include <stdlib.h>
-
-void emptyCluster(cluster_t *cluster) {
-    cluster->points = {}; // reset the array of points
-    cluster->centroid = NULL;
-}
