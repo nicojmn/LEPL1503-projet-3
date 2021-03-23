44c36c9a197eb3a894df2894715633e973a1dcfb
NicoJmn <Nicolas.jeanmenne@student.uclouvain.be>
2021-03-23 12:04:57 +0100

    clean distance.c

--- src/distance.c
+++ src/distance.c
@@ -9,7 +9,7 @@ int64_t absVal(int64_t x) {
 int64_t squared_manhattan_distance(const point_t *p1, const point_t *p2) {
     int64_t result = 0;
     for (int i = 0; i < p1->dimension; ++i) {
-        result += (absVal(p1->vector[i]) - absVal(p2->vector[i])) * (absVal(p1->vector[i]) - absVal(p2->vector[i])); // sum
+        result += absVal(p1->vector[i] - p2->vector[i]); // sum
     }
     return result * result; // square the result to respect the formula
 }
