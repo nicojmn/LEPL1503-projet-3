5c2b0285e0f02c2311c0142f14d97319a0293b36
buzzy <gilles.maes@student.uclouvain.be>
2021-04-03 17:55:15 +0200

    rewriting output file initialization and writing

--- src/createOutputFile.c
+++ src/createOutputFile.c
@@ -7,7 +7,7 @@
 //TODO make tests for csvFileHeadline function
 void csvFileHeadline(bool quiet, FILE *outputPath){
     char *dataNames = malloc(46*sizeof(char));
-    dataNames = "initialization centroids,distortion,centroids";
+    dataNames = "initialization centroids,distortion,centroids"; /*45*/
     fprintf(outputPath, "%s", dataNames);
 
     if (!quiet) {
@@ -36,4 +36,35 @@ void writeOneKmeans(k_means_t *kMeans, bool quiet, FILE *outputPath, point_t *st
     fprintf(outputPath,"\n");
 
     free(toWrite); */
+}
+
+int64_t writeOneKmeansBis(bool quiet) {
+    FILE *fpt;
+    fpt = fopen("output.csv", "a+");
+    if (fpt == NULL) {
+        return -1;
+    }
+
+    char onVerification[46];
+    char *dataNames = (char *) malloc(sizeof(char) * 46);
+    if (dataNames == NULL) {
+        return -1;
+    }
+    dataNames = "initialization centroids,distortion,centroids";
+
+
+    fgets(onVerification, sizeof(onVerification), fpt);
+
+    if (!strcmp(onVerification, dataNames)) {
+        fprintf(fpt, "%s", dataNames);
+        if (!quiet) {
+            char *suppDataNames = malloc(9*sizeof(char));
+            suppDataNames = ",clusters";
+            fprintf(fpt, "%s", suppDataNames);
+            free(suppDataNames);
+        }
+    }
+    free(dataNames);
+
+
 }
