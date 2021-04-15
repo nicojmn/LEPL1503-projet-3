fcf13d8b139c0e76931a381c3074325b5e6f835b
Samuel de Meester de Ravestein <samuel.demeester@student.uclouvain.be>
2021-04-15 22:39:16 +0200

    adding manageHeap.h
    co-authored-by: Loic Spigeleer <loic.spigeleer@student.uclouvain.be>

---
+++ headers/manageHeap.h
@@ -0,0 +1,22 @@
+#ifndef MANAGEHEAP_H
+#define MANAGEHEAP_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "kMeans.h"
+
+typedef struct {
+    FILE *input_stream;
+    FILE *output_stream;
+    uint32_t n_threads;
+    uint32_t k;
+    uint32_t n_first_initialization_points;
+    bool quiet;
+    squared_distance_func_t squared_distance_func;
+} args_t;
+
+void clean(k_means_t *KMeans);
+
+void fullClean(data_t *generalData, point_t **startingCentroids, uint64_t iterationNumber, args_t args);
+
+#endif
