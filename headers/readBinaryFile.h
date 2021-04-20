85553cc3c41fb2851caba862bba3920a56c8a8df
NicoJmn <nicolas.jeanmenne@student.uclouvain.be>
2021-04-20 11:14:10 +0200

    add docstring

--- headers/readBinaryFile.h
+++ headers/readBinaryFile.h
@@ -9,11 +9,12 @@
 #include <stdlib.h>
 
 #include "kMeans.h"
+
 /**
- * #TODO Signature
- * @param file
- * @param data
- * @return
+ *
+ * @param file : the binary file to read
+ * @param data : the data read from the file
+ * @return 0 if load succeed, -1 if error occurs
  */
 int32_t loadData(FILE *file, data_t *data);
 
