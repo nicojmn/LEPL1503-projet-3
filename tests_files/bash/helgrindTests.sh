bea9df35fdfed5fa257693846492c9a6faf15783
NicoJmn <nicolas.jeanmenne@student.uclouvain.be>
2021-05-15 19:08:53 +0200

    Changing number of runs for smoother reading

--- tests_files/bash/helgrindTests.sh
+++ tests_files/bash/helgrindTests.sh
@@ -8,21 +8,19 @@ echo "==========================
 ||  Version without -q  ||
 =========================="
 
-for nThreads in 1 2 3 4 5 6 7 8; do
+for nThreads in 1 4 8; do
   echo " | | | | | Runnig helgrind test with $nThreads thread(s) | | | | |"
   valgrind --tool=helgrind ./kmeans -k 2 -p 3 -n $nThreads -t -d euclidean -f tests_files/output_csv/kmeans.csv \
-        tests_files/input_binary/ex3.bin
+    tests_files/input_binary/ex3.bin
   echo "Done with $nThreads thread(s)"
 done
-
-
 
 echo "=======================
 ||  Version with -q  ||
 ======================="
-for qNThreads in 1 2 3 4 5 6 7 8; do
+for qNThreads in 1 4 8; do
   echo "| | | | | Runnig helgrind test with $qNThreads thread(s) | | | | |"
   valgrind --tool=helgrind ./kmeans -k 2 -p 3 -n $qNThreads -t -q -d euclidean -f tests_files/output_csv/kmeans.csv \
-          tests_files/input_binary/ex3.bin
+    tests_files/input_binary/ex3.bin
   echo "| | | | | Done with $qNThreads thread(s) | | | | |"
 done
