cac5a90b9308d9b412360dcce5f252949d65a0ea
Sébastien Mary <sebastien.mary@student.uclouvain.be>
2021-05-16 00:37:36 +0200

    Delete one to big test

--- tests_files/bash/compareWithPythonTests.sh
+++ tests_files/bash/compareWithPythonTests.sh
@@ -49,9 +49,6 @@ runOneComparison 3 8 4 euclidean ex3.bin
 # Comparison using the file ex6_dim5.bin
 runOneComparison 2 4 4 euclidean ex6_dim5.bin
 
-# Comparison using the file ex7_50Kpoints_dim5.bin
-
-runOneComparison 3 4 7 euclidean ex7_50Kpoints_dim5.bin
 
 # We delete the previous file
 rm "tests_files/comparisonResult.txt"
