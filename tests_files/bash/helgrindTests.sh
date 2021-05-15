355c1a109f713f5d22ab18bd17a1548840d68d9b
NicoJmn <nicolas.jeanmenne@student.uclouvain.be>
2021-05-15 18:15:01 +0200

    Increase coverage of helgrindTests.sh

--- tests_files/bash/helgrindTests.sh
+++ tests_files/bash/helgrindTests.sh
@@ -8,12 +8,21 @@ echo "==========================
 ||  Version without -q  ||
 =========================="
 
-valgrind --tool=helgrind ./kmeans -k 2 -p 3 -n 2 -t -d euclidean -f tests_files/output_csv/kmeans.csv \
+for nThreads in 1 2 3 4 5 6 7 8; do
+  echo " | | | | | Runnig helgrind test with $nThreads thread(s) | | | | |"
+  valgrind --tool=helgrind ./kmeans -k 2 -p 3 -n $nThreads -t -d euclidean -f tests_files/output_csv/kmeans.csv \
         tests_files/input_binary/ex3.bin
+  echo "Done with $nThreads thread(s)"
+done
+
+
 
 echo "=======================
 ||  Version with -q  ||
 ======================="
-
-valgrind --tool=helgrind ./kmeans -k 2 -p 3 -n 2 -t -q -d euclidean -f tests_files/output_csv/kmeans.csv \
-        tests_files/input_binary/ex3.bin
+for qNThreads in 1 2 3 4 5 6 7 8; do
+  echo "| | | | | Runnig helgrind test with $qNThreads thread(s) | | | | |"
+  valgrind --tool=helgrind ./kmeans -k 2 -p 3 -n $qNThreads -t -q -d euclidean -f tests_files/output_csv/kmeans.csv \
+          tests_files/input_binary/ex3.bin
+  echo "| | | | | Done with $qNThreads thread(s) | | | | |"
+done
