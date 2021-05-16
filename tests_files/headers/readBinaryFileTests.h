5e5f99c2fe5ff5c85a96565c3b7b106ab3d7033d
NicoJmn <nicolas.jeanmenne@student.uclouvain.be>
2021-05-16 23:28:08 +0200

    add docstrings for setup() and teardown() in all tests

--- tests_files/headers/readBinaryFileTests.h
+++ tests_files/headers/readBinaryFileTests.h
@@ -7,8 +7,16 @@
 
 #include "../../headers/readBinaryFile.h"
 
+/**
+ * Function to prepare tests (malloc(), assigning variables)
+ * @return 0 if everything goes well, -1 if an error occurs
+ */
 int32_t setupBinaryFile(void);
 
+/**
+ * Function to clean memory after tests
+ * @return 0
+ */
 int32_t teardownBinaryFile(void);
 
 void testReadBinaryFile(void);
